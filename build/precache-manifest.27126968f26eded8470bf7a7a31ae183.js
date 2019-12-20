98280610db476f38a9aa2134eddec8e4e370544e
unknown <naormordechaix@gmail.com>
2019-12-20 14:30:27 +0200

    added first build folder

---
+++ build/precache-manifest.27126968f26eded8470bf7a7a31ae183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf2e2d583f7979531284f5d633d011b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc39ae7ebfc31b09c505",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "99ca435028611f19aa5b",
+    "url": "/static/js/2.08d113c1.chunk.js"
+  },
+  {
+    "revision": "bc39ae7ebfc31b09c505",
+    "url": "/static/js/main.74c3953a.chunk.js"
+  },
+  {
+    "revision": "8a5f184396da696cd615",
+    "url": "/static/js/runtime-main.aaa1a2c4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
